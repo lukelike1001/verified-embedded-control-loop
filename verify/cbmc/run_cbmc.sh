1fa008f0b9b885830292d21430db46b257dd3d9f
Luke Nam <lukelike1001@gmail.com>
2025-11-04 12:57:28 -0800

    REFACTOR-01: Remove redundant calls by defining COMMON_FLAGS for script

--- verify/cbmc/run_cbmc.sh
+++ verify/cbmc/run_cbmc.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cbmc \
-    verify/cbmc/control_loop_cbmc.c \
-    src/control_loop.c \
-    --bounds-check \
-    --pointer_check \
-    --nan-check \
-    --floatbv \
-    --unwind 2 \
-    --unwinding-assertions
+COMMON_FLAGS = "--bounds-check --pointer_check --nan-check --floatbv --unwind 2 --unwinding-assertions"
+
+# FORMAL-01: Verify a single-step harness
+cbmc verify/cbmc/control_loop_cbmc.c src/control_loop.c
