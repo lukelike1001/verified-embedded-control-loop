392b9ed427e927d358e27cc8761b6412cc760115
Luke Nam <lukelike1001@gmail.com>
2025-11-04 12:59:06 -0800

    Fix FEATURE-01: Add COMMON_FLAGS to run the script properly

--- verify/cbmc/run_cbmc.sh
+++ verify/cbmc/run_cbmc.sh
@@ -4,4 +4,4 @@ set -e
 COMMON_FLAGS = "--bounds-check --pointer_check --nan-check --floatbv --unwind 2 --unwinding-assertions"
 
 # FORMAL-01: Verify a single-step harness
-cbmc verify/cbmc/control_loop_cbmc.c src/control_loop.c
+cbmc verify/cbmc/control_loop_cbmc.c src/control_loop.c $COMMON_FLAGS
